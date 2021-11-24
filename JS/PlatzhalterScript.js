693924c2ed7209e90a0a2783aa98167ed8d1a879
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-11-24 10:13:49 +0100

    js datei angelegt, zusammenfassung und aufwärmen test

---
+++ JS/PlatzhalterScript.js
@@ -0,0 +1,16 @@
+
+
+/*global $, document*/
+$(document).ready(function () {
+    "use strict";
+    $(".zusammenfassung").click(function () {
+        $(".zusammenfassungtoggle").slideToggle();
+    });
+});
+
+$(document).ready(function () {
+    "use strict";
+    $(".aufwärmen").click(function () {
+        $(".aufwärmentoggle").slideToggle();
+    });
+});
