c35b288a3ff5a040a74755356cadeca9fb55ea31
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-11-24 16:35:09 +0100

    new design features added in example scene

--- JS/PlatzhalterScript.js
+++ JS/PlatzhalterScript.js
@@ -6,11 +6,19 @@ $(document).ready(function () {
     $(".zusammenfassung").click(function () {
         $(".zusammenfassungvideo,p1").slideToggle();
     });
-});
+    $(".zusammenfassung").click(function () {
+        var angle = 0;
+        setInterval(function () {
+            angle += 90;
+            $(".zusammenfassungArrow").rotate(angle);
+        });
+    });
 
-$(document).ready(function () {
-    "use strict";
+
+
     $(".aufwaermen").click(function () {
         $(".aufwaermenvideo,p2").slideToggle();
+        
     });
-});
+    
+}); //ready end
