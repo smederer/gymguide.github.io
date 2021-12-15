cc743df02d443d6e602b015e278a22c334780a25
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2021-12-15 12:47:14 +0100

    redesign start - hyperlinks+ani, bugfix

--- JS/PlatzhalterScript.js
+++ JS/PlatzhalterScript.js
@@ -2,57 +2,14 @@
 
 /*global $, document*/
 $(document).ready(function () {
-    "use strict";
-    
-    var x = true;
-    $(".zusammenfassung").click(function () {
-        if (x === true) {
-            $(".zusammenfassungvideo,p1").slideDown();
-            $(".zusammenfassungArrow").rotate({
-                duration: 800,
-                angle: 0,
-                animateTo: 90
-            });
-            x = false;
-        } else {
-            $(".zusammenfassungvideo,p1").slideUp();
-            $(".zusammenfassungArrow").rotate({
-                duration: 800,
-                angle: 90,
-                animateTo: 0
-            });
-            x = true;
-            
-        }
-    });
-    
-    
-    
-    
-    var z = true;
-    
-    
-    $(".aufwaermen").click(function () {
-        if (z === true) {
-            $(".aufwaermenvideo,p2").slideDown();
-            $(".aufwaermenArrow").rotate({
-                duration: 800,
-                angle: 0,
-                animateTo: 90
-            });
-            z = false;
-        } else {
-            $(".aufwaermenvideo,p2").slideUp();
-            $(".aufwaermenArrow").rotate({
-                duration: 800,
-                angle: 90,
-                animateTo: 0
-            });
-            z = true;
-        }
-    });
+    $.localScroll({
+        offset: -80,
+        duration: 350,
+    }
+    );
     
    
 
     
-}); //ready end
+});
+//ready end
