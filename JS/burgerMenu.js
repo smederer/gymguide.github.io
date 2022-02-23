f754251b7f2533a0d07a223b3778be6f25f73a93
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2022-02-23 17:17:08 +0100

    Burger BC change

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
   const hamburger = document.querySelector(".Hamburger");
   const navMenu = document.querySelector(".nav-menu");
+  const BC = document.querySelector(".wrapperMain");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    BC.classList.toggle("active");
   });
 });
