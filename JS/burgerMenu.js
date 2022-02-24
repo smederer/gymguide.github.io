8a6ee2d5f696722b18452b5dc2ce6fe008fd8a83
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2022-02-24 17:50:29 +0100

    header resizing, strich ani

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -3,11 +3,16 @@ $(document).ready(function () {
   const navMenu = document.querySelector(".nav-menu");
   //const BC = document.querySelector(".blackbackground");
   const headerChange = document.querySelector(".LogoFont");
+  const lineOpacity = document.getElementsByClassName("line");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     //BC.classList.toggle("active");
     headerChange.classList.toggle("active");
+
+    for (let i = 0; i < lineOpacity.length; i++) {
+      lineOpacity[i].classList.toggle("active");
+    }
   });
 });
