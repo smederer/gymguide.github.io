d538baff18183e8c4919e603c4bb2a96f99ed166
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2022-02-24 17:15:30 +0100

    LinesInNav

--- JS/burgerMenu.js
+++ JS/burgerMenu.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
   const hamburger = document.querySelector(".Hamburger");
   const navMenu = document.querySelector(".nav-menu");
-  const BC = document.querySelector(".blackbackground");
+  //const BC = document.querySelector(".blackbackground");
+  const headerChange = document.querySelector(".LogoFont");
 
   hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-    BC.classList.toggle("active");
+    //BC.classList.toggle("active");
+    headerChange.classList.toggle("active");
   });
 });
