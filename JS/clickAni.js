aa4907716e0ed470870f02d400905f93d97c9827
smederer <sebastian.mederer03@htl-salzburg.ac.at>
2022-03-23 11:50:07 +0100

    Update 23.03

---
+++ JS/clickAni.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $(".platzhalter02").click(function () {
+    console.log("hi");
+    $(this).css({ border: "solid #fecd25" });
+  });
+});
+
+/*$("#hyperlinkAuf").click(function () {
+    $("#hyperlinkAuf").fadeToggle(100).fadeToggle(100);
+  });
+  $("#hyperlinkAus").click(function () {
+    $("#hyperlinkAus").fadeToggle(100).fadeToggle(100);
+  });
+  $("#hyperlinkVo").click(function () {
+    $("#hyperlinkVo").fadeToggle(100).fadeToggle(100);
+  });*/
---
+++ JS/scrollScript.js
@@ -0,0 +1,8 @@
+/*global $, document*/
+$(document).ready(function () {
+  $.localScroll({
+    offset: -100,
+    duration: 400,
+  });
+});
+//ready end
